0414d3f1ea0da12a511c209afd31ae6b3594e698
PALEGARU ROOPESH NAIDU <palegariroopeshnaidu@gmail.com>
2025-11-14 19:15:29 +0530

    Initial commit

---
+++ src/test/java/com/krish/voicecatlogagent/VoicecatlogagentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krish.voicecatlogagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoicecatlogagentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
